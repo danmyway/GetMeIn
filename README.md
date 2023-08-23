f8c0b629040cc612ac2e916f348c575de9942193
Daniel Diblik <ddiblik@redhat.com>
2023-08-23 12:08:19 +0200

    Prototype PoC
    * Deployment of CentOS 7, Alma 8 and Rocky 8 on GCP
    * other features covered in README.md
    
    Signed-off-by: Daniel Diblik <ddiblik@redhat.com>

---
+++ README.md
@@ -0,0 +1,113 @@
+# GetMeIn
+A utility to easily deploy Alma Linux 8, Rocky Linux 8 and CentOS 7 on GCP.
+
+More information will follow.
+
+Please refer to --help.
+
+## Usage 
+
+#### Available images
+Deploys instances of official free images of:
+* Rocky Linux 8
+* Alma Linux 8
+* CentOS 7
+
+The utility checks for the latest version of the image available. \
+IIUIC these should be with the latest updates.\
+As far as the utility is concerned, it should be able to handle deploying other images with the `IMAGE_MAPPING` updated with valid values.
+
+**Schema example:**
+```shell
+{
+  "os_alias": {
+    "project": "rhel-9",
+    "image_family": "rhel-cloud"
+  }
+}
+```
+See [the official documentation](https://cloud.google.com/sdk/gcloud/reference/compute/images/describe-from-family) for more context.
+>**__WARNING!__**  
+> This has not been tested!
+
+#### VM Specification
+By default, the instance is created with these specifications:
+* Machine type: `e2-medium`
+  * Mem 4 GB
+  * 1-2 vCPU (1 shared core)
+* Disk Size
+  * 20 GB
+
+Can be modified if deemed unsatisfactory.
+
+### Prerequisites
+
+>**__IMPORTANT:__**  
+> Right now the configuration secret values are not very well handled. \
+> Please copy the template config file from the assets directory of the repository.\
+> Reach out to me or access the team BitWarden collection and look for the getmein.conf note for the configuration values.
+```commandline
+cp assets/getmein.conf ~/.config/getmein.conf
+```
+
+### alias
+Set an alias to ease working with the utility.
+By default, creates a `GetMeIn` alias in `~/.bashrc`
+Can be customized with `--custom`.
+
+>**__NOTE:__**  
+> The following documentation considers the alias to be set to `GetMeIn`.
+
+### init
+ 
+With the config file set up, you can initialize and authenticate the script running:
+```shell
+GetMeIn init
+```
+This will add the `google-cloud-sdk.repo` file to your system and install the `google-cloud-cli` package from there.\
+If you have the repofile and/or the package present on your system already, nothing will happen.\
+The script will authenticate to the Google Cloud SDK, browser window will pop up asking you for acknowledgement and permission. \
+Again, in case you already have a credentialed account, nothing will happen. \
+You can (however do not need to) verify prior by running `gcloud auth list`
+Lastly, the project_id and zone will be set from the config file (if set up already).
+The zone value has a default value hardcoded in the config template.
+
+### start
+```shell
+# Get Alma Linux 8 instance
+GetMeIn start my-alma-instance alma8
+
+# Get Rocky Linux 8 instance
+GetMeIn start my-rocky-instance rocky8
+
+# Get CentOS 7 instance
+GetMeIn start my-centos-instance centos7
+```
+
+If called with `--ssh` the script should automatically resolve root login via ssh issues by calling `gcloud compute ssh root@<instance_name>`.\
+This call generates ssh keys at ~/.ssh/google_compute_engine(.pub), injects the key to the `/root/.ssh/authorized_keys` on the host machine and connects you to the machine, if you have the ssh keys already on your system, the utility will just inject them to the host machine.\
+
+>**__NOTE:__**  
+> There is a 60-second sleep time between deploying the instance and trying to ssh into it.\
+> In most cases that is enough time for the guest system to be able to accept the ssh connection.\
+Rarely the ssh will fail, but can be repeated by running either `gcloud compute ssh root@<instance_name>` or `ssh -i ~/.ssh/google_compute_engine root@<instance_ip_address>`
+
+The usual way of connecting through the ssh works as well.
+```shell
+ssh -i ~/.ssh/google_compute_engine root@<instance_ip_address>
+
+# Or add the key to the ssh agent
+ssh-add ~/.ssh/google_compute_engine
+ssh root@<instance_ip_address>
+```
+
+You can list deployed instances by running `gcloud compute instances list`.
+The utility won't allow you to deploy two instances with the same name. 
+
+### ssh
+In case you want to deploy multiple instances, you do not need to resolve the ssh straight away.
+Instead, you can just deploy the machines and utilize the `ssh` subcommand when ready.
+
+```shell
+GetMeIn ssh <instance_name>
+```
---
+++ gcp.py
@@ -0,0 +1,235 @@
+#!/usr/bin/env python3
+
+import os.path
+import re
+import subprocess
+import sys
+import time
+
+from __main__ import ARGS, CONFIG_PATH, IMAGE_MAPPING, LOGGER
+from __main__ import get_config
+
+
+def initialize_gcloud():
+    """
+    Initialize the connection to the GCP, set basic information (project_id, zone)
+    from either commandline arguments or config file.
+    :return:
+    """
+    gcloud_repo_path = "/etc/yum.repos.d/google-cloud-sdk.repo"
+    # Add the google-cloud-sdk.repo
+    gcloud_repo_content = "assets/google-cloud-sdk.repo"
+    # Check if google-cloud-sdk.repo exists, create if not
+    LOGGER.info("> INIT: Checking if the google-cloud-sdk.repo is present.")
+    if not os.path.exists(gcloud_repo_path):
+        LOGGER.info("> INIT: Adding google-cloud-sdk repository")
+        copy_repo_command = ["cp", gcloud_repo_content, gcloud_repo_path]
+        LOGGER.debug(f"Running command: {copy_repo_command}")
+        subprocess.run(copy_repo_command, shell=True, check=True)
+    LOGGER.info("> INIT: google-cloud-sdk.repo is added.")
+    # Install google-cloud-cli
+    install_gcloud_pkg = ["sudo", "dnf", "install", "google-cloud-cli"]
+    # Check if the package is not already installed first
+    try:
+        subprocess.run(
+            ["rpm", "-q", "google-cloud-cli"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            check=True,
+        )
+    except subprocess.CalledProcessError:
+        LOGGER.info("> INIT: Installing google-cloud-cli package")
+        LOGGER.debug(f"Running command: {install_gcloud_pkg}")
+        if ARGS.y:
+            LOGGER.info("> INIT: Proceeding with the installation.")
+            install_gcloud_pkg += " -y"
+        subprocess.run(install_gcloud_pkg, check=True)
+    # Initialize gcloud
+    zone, project_id, _ = get_config()
+    if not zone or not project_id:
+        LOGGER.warning(
+            f"No value for project_id and/or zone provided.\nPlease use --zone, --project_id or modify the config file at {CONFIG_PATH}.\nExiting"
+        )
+        sys.exit(99)
+    # Authorize to gcloud
+    list_auth = subprocess.run(
+        ["gcloud", "auth", "list"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+    )
+    if "No credentialed accounts" in list_auth.stderr:
+        subprocess.run(["gcloud", "auth", "login"], check=True)
+
+    # Set project to either config or argument value
+    subprocess.run(["gcloud", "config", "set", "project", project_id], check=True)
+    # Set zone to either config or argument value
+    subprocess.run(["gcloud", "config", "set", "compute/zone", zone], check=True)
+
+
+def set_config(project_id=None, zone=None):
+    """
+    Change the configuration file from command line.
+    """
+    if ARGS.zone:
+        zone = ARGS.zone
+
+    elif ARGS.project_id:
+        project_id = ARGS.project_id
+
+    elif ARGS.config:
+        zone, project_id, _ = get_config()
+
+    subprocess.run(["gcloud", "config," "set", "compute/zone", zone], check=True)
+    subprocess.run(["gcloud", "config," "set", "project", project_id], check=True)
+
+
+def create_instance(instance_name):
+    """
+    Create instance on GCP
+    :param instance_name:
+    :return:
+    """
+    zone, project_id, service_account = get_config()
+
+    startup_script = """sudo sed -i '/^#PermitRootLogin/s/^#//' /etc/ssh/sshd_config\nsudo sed -i '/^PermitRootLogin.*/s/.*/PermitRootLogin yes/' /etc/ssh/sshd_config\nsudo systemctl restart sshd"""
+    if ARGS.startup_script:
+        startup_script = ARGS.startup_script
+
+    def _get_latest_image(requested_os):
+        image_family = IMAGE_MAPPING.get(requested_os)["image_family"]
+        image_project = IMAGE_MAPPING.get(requested_os)["project"]
+        latest_image_command = [
+            "gcloud",
+            "compute",
+            "images",
+            "describe-from-family",
+            image_family,
+            f"--project={image_project}",
+        ]
+        LOGGER.info(
+            f"> START: Getting the latest image for the requested OS {requested_os}"
+        )
+        try:
+            result = subprocess.run(
+                latest_image_command,
+                check=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True,
+            )
+            # Extract the value of the 'name' field using regex
+            name_field_pattern = r"name: (.+)"
+            name_field_match = re.search(name_field_pattern, result.stdout)
+            latest_image = name_field_match.group(1)
+            LOGGER.info(f"> START: Latest image found to be {latest_image} !")
+        except:
+            raise
+
+        image_string = f"projects/{image_project}/global/images/{latest_image}"
+
+        return image_string
+
+    def _is_unique():
+        """ """
+        LOGGER.info(
+            f"> START: Verifying that the {instance_name} instance doesn't exist."
+        )
+        list_instance_command = subprocess.run(
+            [
+                "gcloud",
+                "compute",
+                "instances",
+                "list",
+                "--filter",
+                f"name~'^{instance_name}$'",
+            ],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+        )
+        # The command returns "Listed 0 items." on STDERR if the instance name is not matched
+        if not list_instance_command.stderr:
+            LOGGER.critical(f"The instance of the name {instance_name} already exists.")
+            LOGGER.critical(
+                "Please provide different name for the deployed instance, or use the existing one."
+            )
+            LOGGER.critical(
+                f"The conflicting instance info:\n{list_instance_command.stdout}"
+            )
+            LOGGER.critical("The utility will exit now.")
+            sys.exit(99)
+        return True
+
+    _is_unique()
+
+    # Create an instance with a startup script
+    image = _get_latest_image(ARGS.requested_os)
+
+    create_instance_command = [
+        "gcloud",
+        "compute",
+        "instances",
+        "create",
+        instance_name,
+        f"--project={project_id}",
+        f"--zone={zone}",
+        "--machine-type=e2-medium",
+        f"--metadata=startup-script={startup_script}",
+        "--maintenance-policy=MIGRATE",
+        "--provisioning-model=STANDARD",
+        f"--service-account={service_account}",
+        "--scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append",
+        f"--create-disk=auto-delete=yes,boot=yes,device-name={instance_name},image={image},mode=rw,size=20,type=projects/leapp-devel-700/zones/us-central1-a/diskTypes/pd-balanced",
+        "--no-shielded-secure-boot",
+        "--shielded-vtpm",
+        "--shielded-integrity-monitoring",
+        "--labels=goog-ec-src=vm_add-gcloud",
+        "--reservation-affinity=any",
+    ]
+
+    LOGGER.info(f"> START: Creating the instance {instance_name}")
+
+    subprocess.run(create_instance_command, check=True)
+
+    if ARGS.ssh:
+        _instance_ssh(ARGS.instance_name)
+
+
+def _instance_ssh(instance_name):
+    #
+
+    wait_time = 60
+    gcloud_ssh_command = "gcloud", "compute," "ssh", f"root@{instance_name}"
+    gcloud_ssh_key_path = os.path.expanduser("~/.ssh/google_compute_engine")
+    LOGGER.info(f"Waiting for {wait_time} seconds for the instance to become alive.")
+    while wait_time > 0:
+        sys.stdout.write(f"\rWaiting: {wait_time} seconds remaining. ")
+        sys.stdout.flush()
+        time.sleep(1)  # Sleep for 1 second
+        wait_time -= 1
+    print("\n")
+    LOGGER.info(
+        f"> START: The script will now call the '{gcloud_ssh_command}' command."
+    )
+    LOGGER.info(
+        f"> START: This will generate new gcloud ssh keys under {gcloud_ssh_key_path}(.pub)\nand inject to the /root/.ssh/authorized_keys"
+    )
+    if os.path.exists(gcloud_ssh_key_path) and os.path.exists(
+        f"{gcloud_ssh_key_path}.pub"
+    ):
+        LOGGER.info(
+            "> START: The ssh keys found at the default location, they won't be regenerated."
+        )
+    response = input("Do you want to continue with the automation? (Y/n)")
+    while response.lower not in ("y", "n"):
+        if response.lower() == "y":
+            LOGGER.info("Proceeding with the gcloud ssh command.")
+            subprocess.run(gcloud_ssh_command, check=True)
+            return
+        elif response.lower() == "n":
+            LOGGER.info("Exiting.")
+            sys.exit(99)
+        else:
+            response = input("Please provide a valid answer. (Y/n)")
